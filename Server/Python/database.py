ef2cbd323ea8ea29a1346ffa850267abb0e4930a
Ybz1234 <jonathanbz49@gmail.com>
2024-08-15 14:39:26 +0300

    Delete all Python code that is not API related.

--- Server/Python/database.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-
-def get_mongo_client(connection_string):
-    return MongoClient(connection_string)
-
-def fetch_user_data(client, database_name, collection_name):
-    db = client[database_name]
-    collection = db[collection_name]
-    return list(collection.find({}, {'_id': 0, 'firstName': 1, 'lastName': 1, 'email': 1, 'role': 1}))
