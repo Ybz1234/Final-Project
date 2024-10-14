259753cd73c9ef513d30a16560c207aa306c881a
inonvv <115711486+inonvv@users.noreply.github.com>
2024-10-14 18:47:11 +0300

    vertical card implemet ani memamesh OTO

--- Client/screens/FullTrip.js
+++ Client/screens/FullTrip.js
@@ -2,7 +2,7 @@ import { StyleSheet, View, Text, ScrollView } from "react-native";
 import React, { useState, useEffect } from "react";
 import CardsSlide from "../components/CardsSlide";
 import PageFrame from "../components/PageFrame";
-import { Card, Button } from "react-native-paper";
+import { Card, Button, Modal, Portal, Provider } from "react-native-paper";
 
 const FullTrip = ({ route, navigation }) => {
   const { daysArray } = route.params;
@@ -13,6 +13,11 @@ const FullTrip = ({ route, navigation }) => {
   const [selectedIndex, setSelectedIndex] = useState(null);
   const [hotelData, setHotelData] = useState(null);
   const [attractionData, setAttractionData] = useState(null);
+  const [destinations, setDestinations] = useState([]);
+  const [hotelModalVisible, setHotelModalVisible] = useState(false);
+  const [attractionModalVisible, setAttractionModalVisible] = useState(false);
+  const [alternativeHotels, setAlternativeHotels] = useState([]);
+  const [alternativeAttractions, setAlternativeAttractions] = useState([]);
 
   useEffect(() => {
     fetchFlightDetails();
@@ -92,8 +97,8 @@ const FullTrip = ({ route, navigation }) => {
           `Hotel request failed with status: ${hotelResponse.status}`
         );
       }
-      const hotelData = await hotelResponse.json();
-
+      const hotelDataJson = await hotelResponse.json(); // Renamed variable
+      setHotelData(hotelDataJson.hotel[0]);
       const attractionResponse = await fetch(
         "https://final-project-sqlv.onrender.com/api/attractions/findAttractionsByCity",
         {
@@ -112,13 +117,9 @@ const FullTrip = ({ route, navigation }) => {
         );
       }
 
-      const attractionData = await attractionResponse.json();
+      const attractionDataJson = await attractionResponse.json(); // Renamed variable
+      setAttractionData(attractionDataJson.attractions[0]);
 
-      // Store the first hotel and attraction
-      setHotelData(hotelData.hotel[0]);
-      setAttractionData(attractionData.attractions[0]);
-
-      // Show the vertical cards
       setShowVerticalCards(true);
     } catch (error) {
       console.error("Error fetching data:", error.message);
@@ -180,6 +181,69 @@ const FullTrip = ({ route, navigation }) => {
       console.error("Error fetching attractions:", error.message);
     }
   };
+  const selectHotelAlternative = (hotel) => {
+    setHotelData(hotel);
+    setHotelModalVisible(false);
+  };
+  const selectAttractionAlternative = (attraction) => {
+    setAttractionData(attraction);
+    setAttractionModalVisible(false);
+  };
+
+  const handleAlternativeHotels = async () => {
+    setHotelModalVisible(true);
+    try {
+      const hotelResponse = await fetch(
+        "https://final-project-sqlv.onrender.com/api/hotels/findHotelsByCity",
+        {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ city: selectedFlight.arrivalCity }),
+        }
+      );
+
+      if (!hotelResponse.ok) {
+        throw new Error(
+          `Hotel request failed with status: ${hotelResponse.status}`
+        );
+      }
+
+      const hotelDataJson = await hotelResponse.json();
+      setAlternativeHotels(hotelDataJson.hotel);
+    } catch (error) {
+      console.error("Error fetching alternative hotels:", error.message);
+    }
+  };
+  const handleAlternativeAttractions = async () => {
+    setAttractionModalVisible(true);
+    try {
+      const attractionResponse = await fetch(
+        "https://final-project-sqlv.onrender.com/api/attractions/findAttractionsByCity",
+        {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ city: selectedFlight.arrivalCity }),
+        }
+      );
+
+      if (!attractionResponse.ok) {
+        throw new Error(
+          `Attraction request failed with status: ${attractionResponse.status}`
+        );
+      }
+
+      const attractionDataJson = await attractionResponse.json();
+      setAlternativeAttractions(attractionDataJson.attractions);
+    } catch (error) {
+      console.error("Error fetching alternative attractions:", error.message);
+    }
+  };
 
   return (
     <PageFrame>
@@ -200,7 +264,7 @@ const FullTrip = ({ route, navigation }) => {
             {showVerticalCards && (
               <Card style={styles.card}>
                 <Card.Title
-                  title={`${selectedFlight.arrivalCity}: Information`}
+                  title={`Destination: ${selectedFlight.arrivalCity}`}
                   titleStyle={styles.cardTitle}
                 />
                 <Card.Content>
@@ -210,40 +274,48 @@ const FullTrip = ({ route, navigation }) => {
                   >
                     {/* Hotel Card */}
                     {hotelData && (
-                      <Card style={styles.innerCard}>
+                      <Card style={styles.verticalCard}>
                         <Card.Title
-                          title="Hotel Information"
+                          title="Hotel"
                           titleStyle={styles.cardTitle}
                         />
                         <Card.Content>
-                          <Text style={styles.cardText}>
-                            Name: {hotelData.name}
-                          </Text>
-                          <Text style={styles.cardText}>
-                            Address: {hotelData.address.full_address}
-                          </Text>
+                          <Text style={styles.cardText}>Name: </Text>
+                          <Text>{hotelData.name}</Text>
+
+                          <Text style={styles.cardText}> Address: </Text>
+                          <Text>{hotelData.address.full_address}</Text>
                         </Card.Content>
-                        <Button>Alternative</Button>
+                        <Button
+                          onPress={handleAlternativeHotels}
+                          style={styles.button2}
+                          labelStyle={styles.buttonLabel}
+                        >
+                          Alternative
+                        </Button>
                       </Card>
                     )}
 
                     {/* Attraction Card */}
                     {attractionData && (
-                      <Card style={styles.innerCard}>
+                      <Card style={styles.verticalCard}>
                         <Card.Title
-                          title="Attraction Information "
+                          title="Attraction"
                           titleStyle={styles.cardTitle}
                         />
                         <Card.Content>
-                          <Text style={styles.cardText}>
-                            Name: {attractionData.name}
-                          </Text>
-                          <Text style={styles.cardText}>
-                            Description: {attractionData.description}
-                          </Text>
+                          <Text style={styles.cardText}>Name</Text>
+                          <Text>{attractionData.name}</Text>
+                          <Text style={styles.cardText}> Description</Text>
+                          <Text>{attractionData.description}</Text>
                         </Card.Content>
-
-                        <Button>Alternative</Button>
+                        <Button
+                          onPress={handleAlternativeAttractions}
+                          style={styles.button2}
+                          labelStyle={styles.buttonLabel}
+                        >
+                          Alternative
+                        </Button>
                       </Card>
                     )}
                   </ScrollView>
@@ -255,6 +327,55 @@ const FullTrip = ({ route, navigation }) => {
           <></>
         )}
       </ScrollView>
+      <Portal>
+        {/* Hotel Alternatives Modal */}
+        <Modal
+          visible={hotelModalVisible}
+          onDismiss={() => setHotelModalVisible(false)}
+          contentContainerStyle={styles.modalContainer}
+        >
+          <ScrollView>
+            {alternativeHotels.map((hotel, index) => (
+              <Card key={index} style={styles.modalCard}>
+                <Card.Content>
+                  <Text>{hotel.name}</Text>
+                  <Button
+                    style={styles.modalButton} // Styles the button container
+                    labelStyle={styles.modalButtonLabel}
+                    onPress={() => selectHotelAlternative(hotel)}
+                  >
+                    Select
+                  </Button>
+                </Card.Content>
+              </Card>
+            ))}
+          </ScrollView>
+        </Modal>
+
+        {/* Attraction Alternatives Modal */}
+        <Modal
+          visible={attractionModalVisible}
+          onDismiss={() => setAttractionModalVisible(false)}
+          contentContainerStyle={styles.modalContainer}
+        >
+          <ScrollView>
+            {alternativeAttractions.map((attraction, index) => (
+              <Card key={index} style={styles.modalCard}>
+                <Card.Content>
+                  <Text style={{ text: "bold" }}>{attraction.name}</Text>
+                  <Button
+                    style={styles.modalButton} // Styles the button container
+                    labelStyle={styles.modalButtonLabel}
+                    onPress={() => selectAttractionAlternative(attraction)}
+                  >
+                    Select
+                  </Button>
+                </Card.Content>
+              </Card>
+            ))}
+          </ScrollView>
+        </Modal>
+      </Portal>
       <View style={styles.buttonContainerbot}>
         <Button
           title="Confirm Booking"
@@ -290,16 +411,24 @@ const styles = StyleSheet.create({
     marginTop: 20,
     marginBottom: 20,
   },
+  cardTitle: {
+    color: "#1B3E90",
+    fontSize: 18,
+    fontWeight: "bold",
+    fontFamily: "Roboto-BoldItalic",
+    textDecorationLine: "underline",
+  },
   scrollView: {
     height: 400, // Set a fixed height less than the parent card
   },
+
   cardContainer: {
     flexDirection: "column",
     justifyContent: "flex-start",
   },
   innerCard: {
     fontFamily: "Roboto-MediumItalic",
-    height: 150, // Adjust height to fit within the ScrollView
+    height: 150,
     width: "100%", // Ensure the card fits within the parent width
     alignSelf: "center",
     backgroundColor: "rgba(255, 255, 255, 0.8)",
@@ -328,7 +457,7 @@ const styles = StyleSheet.create({
   },
   button2: {
     width: "50%",
-    paddingVertical: 8,
+    paddingVertical: 2,
     alignItems: "center",
     justifyContent: "center",
     marginBottom: 20,
@@ -343,7 +472,7 @@ const styles = StyleSheet.create({
   },
   buttonLabel: {
     color: "white",
-    fontSize: 16, // Font size
+    fontSize: 14, // Font size
     fontWeight: "bold", // Bold text
     fontFamily: "Roboto-Medium",
   },
@@ -355,18 +484,51 @@ const styles = StyleSheet.create({
     backgroundColor: "rgba(255, 255, 255, 0.8)",
     borderRadius: 15,
     marginBottom: 10,
+    backgroundColor: "white",
     padding: 10,
     borderWidth: 1,
     borderColor: "#1B3E90",
-    shadowColor: "#000",
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.3,
-    shadowRadius: 4,
-    elevation: 5,
+    alignItems: "center",
   },
   cardText: {
     fontSize: 16,
     color: "#1B3E90",
+    marginVertical: 15,
     marginBottom: 5,
+    fontFamily: "Roboto-MediumItalic",
+  },
+  modalContainer: {
+    backgroundColor: "rgba(255, 255, 255, 0.8)",
+    padding: 20,
+    margin: 20,
+    borderRadius: 10,
+  },
+  modalCard: {
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 10,
+    width: "80%",
+    alignSelf: "center",
+    borderRadius: 10,
+    backgroundColor: "white",
+    // padding: ,
+    borderColor: "#1B3E90",
+    borderRadius: 10,
+    marginBottom: 10,
+  },
+  modalButton: {
+    backgroundColor: "#1B3E90",
+    marginVertical: 10,
+    alignSelf: "center",
+    borderRadius: 10,
+    marginBottom: 10,
+    color: "white",
+    // paddingVertical: 2,
+    borderRadius: 25,
+  },
+  modalButtonLabel: {
+    color: "white", // Text color
+    fontSize: 12,
+    fontWeight: "bold",
   },
 });
